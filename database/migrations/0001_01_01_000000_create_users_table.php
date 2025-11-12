a2a783c1e4716c96b3fbe89659594f6a73e45390
HossinELgadra <husayn_2584@limu.edu.ly>
2025-11-12 18:42:15 +0200

    add status column to users table with default value true

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('phone_number', 15)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('status')->default(true);
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
